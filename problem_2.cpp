83236e9011ca065e306231477c6bc7d575931b63
Adam King <adamking.br549@gmail.com>
2018-10-16 14:16:42 -0400

    first commit

---
+++ problem_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+    
+    sum = 0
+    i = 0
+    while (true) {
+        if (fibonacci(i) > 4000000) {
+            break;
+        } //if
+    } //while
+
+    return 0;
+}
+
+int fibonacci
